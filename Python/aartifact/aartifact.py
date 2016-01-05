16dce72e2b30f298aff5a04d4e26dafb339fdcab
Andrei Lapets <alapets@bbn.com>
2016-01-04 20:49:46 -0500

    Initial implementation supporting assumption equations.

--- Python/aartifact/aartifact.py
+++ Python/aartifact/aartifact.py
@@ -23,6 +23,35 @@ class AartifactError(Exception):
     def __str__(self):
         return repr(self.value)
 
+class Context():
+    def __init__(self):
+        self.mod = None
+        self.left = None
+        self.right = None
+
+class Term():
+    def __init__(self, arg = None):
+        if arg is None:
+            self.dict = {():0}
+        elif type(arg) == str:
+            self.dict = {():0}
+            self.dict[arg] = 1
+        elif type(arg) == int:
+            self.dict = {():arg}
+
+    def __add__(self, other):
+        result = Term()
+        for k in set(self.dict.keys()) | set(other.dict.keys()):
+            result[k] = 0
+            if k in self.dict:
+                result[k] += self.dict[k]
+            if k in other.dict:
+                result[k] += other.dict[k]
+        return result
+
+    def __repr__(self):
+        return str(self.dict)
+
 # The ModularArithmetic class is used for verifying an algebraic
 # proof involving common constructs and algebraic manipulations in
 # modular arithmetic.
@@ -40,8 +69,50 @@ class ModularArithmetic():
 
     def process(self, func):
         a = ast.parse(inspect.getsource(func))
-        print(ast.dump(a))
-        return self.verify(a)
+        #print(ast.dump(a))
+        return self.convert(a)
+
+    def convert(self, a, context = Context()):
+        if type(a) == ast.Module:
+            return self.convert(a.body[0], context)
+        elif type(a) == ast.FunctionDef:
+            for e in a.body:
+                context = self.convert(e, context)
+            return True
+        elif type(a) == ast.Expr:
+            return self.convert(a.value, context)
+        elif type(a) == ast.Compare:
+            if len(a.ops) == 2 and type(a.ops[0]) == ast.Eq and type(a.ops[1]) == ast.Is:
+                if type(a.comparators[1]) == ast.Name and a.comparators[1].id == 'Assumption':
+                    l = self.left(a.left, context)
+                    r = self.right(a.comparators[0], context)
+                    return True
+                else:
+                    raise AartifactError("The supplied justification is not supported.")
+        else:
+            raise AartifactError("The expression is not a supported formula.")
+
+    def left(self, a, context):
+        if type(a) == ast.Name:
+            return Term(a.id)
+        elif type(a) == ast.Num:
+            return Term(a.n)
+        elif type(a) == ast.BinOp and type(a.op) == ast.Add:
+            return self.left(a.left, context) + self.left(a.right, context)
+        else:
+            raise AartifactError("The expression is not a supported left-hand equation term.")
+
+    def right(self, a, context):
+        if type(a) == ast.Name:
+            return Term(a.id)
+        elif type(a) == ast.Num:
+            return Term(a.n)
+        elif type(a) == ast.BinOp and type(a.op) == ast.Add:
+            return self.right(a.left, context) + self.right(a.right, context)
+        elif type(a) == ast.BinOp and type(a.op) == ast.Mod and type(a.right) == ast.Num:
+            return self.right(a.left, context)
+        else:
+            raise AartifactError("The expression is not a supported right-hand equation term.")
 
     def verify(self, a):
         pass
