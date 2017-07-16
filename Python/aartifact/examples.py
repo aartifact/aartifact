0e5f75f339c739d1f00b7700340d43bb0721d51f
Andrei Lapets <a@lapets.io>
2017-07-16 14:13:59 -0400

    Add basic usage example.

---
+++ Python/aartifact/examples.py
@@ -0,0 +1,25 @@
+#####################################################################
+## 
+## examples.py
+##
+##   Small examples that illustrate how the aartifact.py module can
+##   be used.
+##
+##
+
+from aartifact import ModularArithmetic
+
+#####################################################################
+## A simple run-time approximation framework.
+##
+
+@ModularArithmetic
+def f():
+    gcd(a,b) == 1 is Assumption
+    x == 3 %(4)   is Assumption
+    x == 3        is Euclid
+
+
+print("Done.")
+
+##eof
